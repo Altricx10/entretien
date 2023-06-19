0af35f17814d2858f6344b0d05a3e6025e7069dd
Jonathan EYMA <jonathan.eyma@iconsultants.fr>
2023-06-19 16:55:57 +0200

    Initial commit

---
+++ entretien/src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Main {
+
+  private static final String ONE = "I";
+  private static final String FIVE = "V";
+  private static final String TEN = "X";
+  private static final String FIFTY = "L";
+  private static final String  HUNDRED = "C";
+  private static final String FIVE_HUNDRED = "D";
+  private static final String THOUSAND = "M";
+
+  public static void main(String[] args) {
+
+
+
+  }
+}
