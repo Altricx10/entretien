e897da018071d67ae4dbc49c037c3dbcd38e8c0c
Jonathan EYMA <jonathan.eyma@iconsultants.fr>
2023-06-19 17:29:20 +0200

    Convert One to thirteen

--- entretien/src/main/java/org/example/Main.java
+++ entretien/src/main/java/org/example/Main.java
@@ -12,7 +12,46 @@ public class Main {
 
   public static void main(String[] args) {
 
+  }
 
+  public String convertTenToThirteen(int number) {
 
+    if (number > 10 && number <= 13) {
+      String number1 = convertI(number - 10);
+      return String.format("X%s", number1);
+    }
+
+    if (number == 10) {
+      return "X";
+    }
+
+    return "";
+  }
+
+  public String convertFiveToEight(int number) {
+
+    if (number > 5 && number <= 8) {
+      String number1 = convertI(number - 5);
+      return String.format("V%s", number1);
+    }
+
+    if (number == 5) {
+      return "V";
+    }
+    return "";
+  }
+
+  public String convertI(int number) {
+
+    if (number > 3) {
+      return "";
+    }
+
+    String result = "";
+    for (int i = 0 ; i < number ; i++) {
+      result = String.format("%s%s", result, "I");
+    }
+
+    return result;
   }
 }
--- entretien/src/test/java/MainTest.java
+++ entretien/src/test/java/MainTest.java
@@ -1,3 +1,131 @@
+import org.example.Main;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class MainTest {
 
+  private Main main = new Main();
+
+
+
+  @Test
+  public void testOne() {
+    // Given one number
+    int number = 1;
+
+    // Then calling #manageI
+    String result = main.convertI(number);
+
+    Assert.assertEquals(result, "I");
+  }
+
+  @Test
+  public void testTwo() {
+    // Given one number
+    int number = 2;
+
+    // Then calling #manageI
+    String result = main.convertI(number);
+
+    Assert.assertEquals(result, "II");
+  }
+
+  @Test
+  public void testThree() {
+    // Given one number
+    int number = 3;
+
+    // Then calling #manageI
+    String result = main.convertI(number);
+
+    Assert.assertEquals(result, "III");
+  }
+
+  @Test
+  public void testFive() {
+    // Given one number
+    int number = 5;
+
+    // Then calling #manageI
+    String result = main.convertFiveToEight(number);
+
+    Assert.assertEquals(result, "V");
+  }
+
+  @Test
+  public void testSix() {
+    // Given one number
+    int number = 6;
+
+    // Then calling #manageI
+    String result = main.convertFiveToEight(number);
+
+    Assert.assertEquals(result, "VI");
+  }
+
+  @Test
+  public void testSeven() {
+    // Given one number
+    int number = 7;
+
+    // Then calling #manageI
+    String result = main.convertFiveToEight(number);
+
+    Assert.assertEquals(result, "VII");
+  }
+
+  @Test
+  public void testEight() {
+    // Given one number
+    int number = 8;
+
+    // Then calling #manageI
+    String result = main.convertFiveToEight(number);
+
+    Assert.assertEquals(result, "VIII");
+  }
+
+  @Test
+  public void testTen() {
+    // Given one number
+    int number = 10;
+
+    // Then calling #manageI
+    String result = main.convertTenToThirteen(number);
+
+    Assert.assertEquals(result, "X");
+  }
+
+  @Test
+  public void testEleven() {
+    // Given one number
+    int number = 11;
+
+    // Then calling #manageI
+    String result = main.convertTenToThirteen(number);
+
+    Assert.assertEquals(result, "XI");
+  }
+
+  @Test
+  public void testTwelve() {
+    // Given one number
+    int number = 12;
+
+    // Then calling #manageI
+    String result = main.convertTenToThirteen(number);
+
+    Assert.assertEquals(result, "XII");
+  }
+
+  @Test
+  public void testThirteen() {
+    // Given one number
+    int number = 13;
+
+    // Then calling #manageI
+    String result = main.convertTenToThirteen(number);
+
+    Assert.assertEquals(result, "XIII");
+  }
 }
